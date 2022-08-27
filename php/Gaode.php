8d121154c7ff6115306d8e335b8be96c59281c4d
鲍亮 <1136718390@qq.com>
2022-08-27 04:39:56 +0000

    update php/Gaode.php.
    Signed-off-by: 鲍亮 <1136718390@qq.com>

--- php/Gaode.php
+++ php/Gaode.php
@@ -4,7 +4,7 @@ require_once("./Curl.php");
 
 class Levi_Gaode
 {
-    const KEY = '244e75e78d7110304a626f6b1c3e5f03';
+    const KEY = 'tianqi_15221373275';
 
     const URL_LIST = array(
         'getWeather' => 'https://restapi.amap.com/v3/weather/weatherInfo'
